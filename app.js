cf268a95d9102c02bf4f51b451270746a0b7e729
Da Kuang <dak@surveymonkey.com>
2016-07-30 22:35:40 -0400

    use global state

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
  /** @jsx React.DOM */
 var PokeBall = React.createClass({
-  getInitialState: function(){
-    return {
-      spinning: false
-    }
-  },
-
   _handleClick: function(){
-    this.setState({
-      spinning: !this.state.spinning
-    })
+    this.props.onClick(this.props.index)
   },
 
   render: function(){
     return (
         <img
           src={this.props.imagePath}
-          className={this.state.spinning ? "spinning" : null}
+          className={this.props.spinning ? "spinning" : null}
           style={{width: this.props.imageWidth, height: this.props.imageHeight}}
           onClick={this._handleClick} />
     )
@@ -24,15 +16,36 @@ var PokeBall = React.createClass({
 })
 
 var App = React.createClass({
+
+  getInitialState: function(){
+    return {
+      spinning: [false, false]
+    }
+  },
+
+  _handlePokeballClick: function(index){
+    var newState = {
+      spinning: [false, false]
+    };
+    newState.spinning[index] = !this.state.spinning[index];
+    this.setState(newState)
+  },
+
   render: function(){
     return (
       <div>
         <h1>Hello Pokemon!</h1>
         <PokeBall
+          index={0}
+          spinning={this.state.spinning[0]}
+          onClick={this._handlePokeballClick}
           imagePath="images/pokeball-1.png"
           imageWidth="300px"
           imageHeight="300px" />
         <PokeBall
+          index={1}
+          spinning={this.state.spinning[1]}
+          onClick={this._handlePokeballClick}
           imagePath="images/pokeball-2.png"
           imageWidth="300px"
           imageHeight="300px" />
