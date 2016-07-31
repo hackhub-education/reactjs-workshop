224e0e5cda49224d0247ee046ebfa6bd47e0fc1a
Da Kuang <dak@surveymonkey.com>
2016-07-30 22:07:01 -0400

    add pokeball component

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
  /** @jsx React.DOM */
+var PokeBall = React.createClass({
+  getInitialState: function(){
+    return {
+      spinning: false
+    }
+  },
+
+  _handleClick: function(){
+    this.setState({
+      spinning: !this.state.spinning
+    })
+  },
+
+  render: function(){
+    return (
+        <img
+          src={this.props.imagePath}
+          className={this.state.spinning ? "spinning" : null}
+          style={{width: this.props.imageWidth, height: this.props.imageHeight}}
+          onClick={this._handleClick} />
+    )
+  }
+})
+
 var App = React.createClass({
   render: function(){
     return (
       <div>
         <h1>Hello Pokemon!</h1>
-        <img src="images/pokeball-1.png" style={{width: "300px", height: "300px"}}></img>
+        <PokeBall
+          imagePath="images/pokeball-1.png"
+          imageWidth="300px"
+          imageHeight="300px" />
       </div>
     )
   }
