e84aa65e59c493059dc2a4b362580e9b686d1125
Da Kuang <dak@surveymonkey.com>
2016-07-30 22:05:31 -0400

    show pokeball in the page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var App = React.createClass({
     return (
       <div>
         <h1>Hello Pokemon!</h1>
+        <img src="images/pokeball-1.png" style={{width: "300px", height: "300px"}}></img>
       </div>
     )
   }
