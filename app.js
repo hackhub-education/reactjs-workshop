d296ef777ef032bf708d90ca4ae9826521c4962a
Da Kuang <dak@surveymonkey.com>
2016-07-30 21:53:35 -0400

    implemente basic app for hello pokemon

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
  /** @jsx React.DOM */
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Hello Pokemon!</h1>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById("app"))
